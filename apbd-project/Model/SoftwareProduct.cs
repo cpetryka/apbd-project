fc004efa3894ce502d4f64cf3bfdd2bb07dbfb22
Cezary Petryka <cezaryp10@gmail.com>
2024-06-26 18:25:05 +0200

    Add a model for software product

---
+++ apbd-project/Model/SoftwareProduct.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace apbd_project.Model;
+
+public class SoftwareProduct
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; } = null!;
+    [Required]
+    [MaxLength(200)]
+    public string Description { get; set; } = null!;
+    [Required]
+    public string Version { get; set; } = null!;
+    [Required]
+    [MaxLength(20)]
+    public string Category { get; set; } = null!; // e.g. Finance, Education, ...
+    [Required]
+    public string Type { get; set; } = null!; // Subscription, One-time purchase, ...
+}
