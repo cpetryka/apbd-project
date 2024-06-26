88fff560efc04ac291b5b7130c642871ae053c8e
Cezary Petryka <cezaryp10@gmail.com>
2024-06-26 18:24:44 +0200

    Add discounts service

---
+++ apbd-project/Service/IDiscountService.cs
@@ -0,0 +1,11 @@
+using apbd_project.Model;
+using apbd_project.Model.Dto;
+
+namespace apbd_project.Service;
+
+public interface IDiscountService
+{
+    Task<GetDiscountDto?> GetDiscountById(int id);
+    Task<IEnumerable<GetDiscountDto>> GetAllDiscounts();
+    Task AddDiscount(AddDiscountDto addDiscountDto);
+}
---
+++ apbd-project/Service/Impl/DiscountService.cs
@@ -0,0 +1,64 @@
+using apbd_project.Data;
+using apbd_project.Model;
+using apbd_project.Model.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace apbd_project.Service.Impl;
+
+public class DiscountService : IDiscountService
+{
+    private readonly ApplicationContext _context;
+
+    public DiscountService(ApplicationContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<GetDiscountDto?> GetDiscountById(int id)
+    {
+        return await _context
+            .Discounts
+            .Where(d => d.Id == id)
+            .Select(d => new GetDiscountDto
+            {
+                Id = d.Id,
+                Name = d.Name,
+                Offer = d.Offer,
+                Amount = d.Amount,
+                StartDate = d.StartDate,
+                EndDate = d.EndDate
+            })
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<GetDiscountDto>> GetAllDiscounts()
+    {
+        return await _context
+            .Discounts
+            .Select(d => new GetDiscountDto
+            {
+                Id = d.Id,
+                Name = d.Name,
+                Offer = d.Offer,
+                Amount = d.Amount,
+                StartDate = d.StartDate,
+                EndDate = d.EndDate
+            })
+            .ToListAsync();
+    }
+
+    public async Task AddDiscount(AddDiscountDto addDiscountDto)
+    {
+        var discount = new Discount
+        {
+            Name = addDiscountDto.Name,
+            Offer = addDiscountDto.Offer,
+            Amount = addDiscountDto.Amount,
+            StartDate = addDiscountDto.StartDate,
+            EndDate = addDiscountDto.EndDate
+        };
+
+        await _context.Discounts.AddAsync(discount);
+        await _context.SaveChangesAsync();
+    }
+}
