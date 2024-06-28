9244764fa422902fe4fe6bf2f8fd9c90a2da576f
Cezary Petryka <cezaryp10@gmail.com>
2024-06-28 06:44:47 +0200

    Add security controller

---
+++ apbd-project/Controller/SecurityController.cs
@@ -0,0 +1,55 @@
+using apbd_project.Model.Dto;
+using apbd_project.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apbd_project.Controller;
+
+[Route("api/[controller]")]
+public class SecurityController : ControllerBase
+{
+    private ISecurityService _securityService;
+
+    public SecurityController(ISecurityService securityService)
+    {
+        _securityService = securityService;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterOrLoginUserDto registerOrLoginUserDto)
+    {
+        var authorizationResult = await _securityService.RegisterAsync(registerOrLoginUserDto);
+
+        if (!authorizationResult.Success)
+        {
+            return BadRequest(authorizationResult.Errors);
+        }
+
+        return Ok(authorizationResult);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(RegisterOrLoginUserDto registerOrLoginUserDto)
+    {
+        var authorizationResult = await _securityService.LoginAsync(registerOrLoginUserDto);
+
+        if (!authorizationResult.Success)
+        {
+            return BadRequest(authorizationResult.Errors);
+        }
+
+        return Ok(new { AccessToken = authorizationResult.AccessToken, RefreshToken = authorizationResult.RefreshToken });
+    }
+
+    [HttpPost("refresh")]
+    public async Task<IActionResult> Refresh(string refreshToken)
+    {
+        var authorizationResult = await _securityService.RefreshTokenAsync(refreshToken);
+
+        if (!authorizationResult.Success)
+        {
+            return BadRequest(authorizationResult.Errors);
+        }
+
+        return Ok(new { AccessToken = authorizationResult.AccessToken });
+    }
+}
